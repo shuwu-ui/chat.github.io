8a0fe841b83b1548f4ca7d31559eda4dd3f2d160
shiwu <152254394+shuwu-ui@users.noreply.github.com>
2024-02-22 13:26:48 +0800

    Update update.ts

--- app/store/update.ts
+++ app/store/update.ts
@@ -1,164 +1,133 @@
-import {
-  FETCH_COMMIT_URL,
-  FETCH_TAG_URL,
-  ModelProvider,
-  StoreKey,
-} from "../constant";
 import { getClientConfig } from "../config/client";
+import { Updater } from "../typing";
+import { ApiPath, STORAGE_KEY, StoreKey } from "../constant";
 import { createPersistStore } from "../utils/store";
-import ChatGptIcon from "../icons/chatgpt.png";
+import {
+  AppState,
+  getLocalAppState,
+  GetStoreState,
+  mergeAppState,
+  setLocalAppState,
+} from "../utils/sync";
+import { downloadAs, readFromFile } from "../utils";
+import { showToast } from "../components/ui-lib";
 import Locale from "../locales";
-import { use } from "react";
-import { useAppConfig } from ".";
-import { ClientApi } from "../client/api";
+import { createSyncClient, ProviderType } from "../utils/cloud";
+import { corsPath } from "../utils/cors";
 
-const ONE_MINUTE = 60 * 1000;
+export interface WebDavConfig {
+  server: string;
+  username: string;
+  password: string;
+}
+
 const isApp = !!getClientConfig()?.isApp;
+export type SyncStore = GetStoreState<typeof useSyncStore>;
 
-function formatVersionDate(t: string) {
-  const d = new Date(+t);
-  const year = d.getUTCFullYear();
-  const month = d.getUTCMonth() + 1;
-  const day = d.getUTCDate();
+const DEFAULT_SYNC_STATE = {
+  provider: ProviderType.WebDAV,
+  useProxy: true,
+  proxyUrl: corsPath(ApiPath.Cors),
 
-  return [
-    year.toString(),
-    month.toString().padStart(2, "0"),
-    day.toString().padStart(2, "0"),
-  ].join("");
-}
-
-type VersionType = "date" | "tag";
-
-async function getVersion(type: VersionType) {
-  if (type === "date") {
-    const data = (await (await fetch(FETCH_COMMIT_URL)).json()) as {
-      commit: {
-        author: { name: string; date: string };
-      };
-      sha: string;
-    }[];
-    const remoteCommitTime = data[0].commit.author.date;
-    const remoteId = new Date(remoteCommitTime).getTime().toString();
-    return remoteId;
-  } else if (type === "tag") {
-    const data = (await (await fetch(FETCH_TAG_URL)).json()) as {
-      commit: { sha: string; url: string };
-      name: string;
-    }[];
-    return data.at(0)?.name;
-  }
-}
-
-export const useUpdateStore = createPersistStore(
-  {
-    versionType: "tag" as VersionType,
-    lastUpdate: 0,
-    version: "unknown",
-    remoteVersion: "",
-    used: 0,
-    subscription: 0,
-
-    lastUpdateUsage: 0,
+  webdav: {
+    endpoint: "",
+    username: "",
+    password: "",
   },
+
+  upstash: {
+    endpoint: "",
+    username: STORAGE_KEY,
+    apiKey: "",
+  },
+
+  lastSyncTime: 0,
+  lastProvider: "",
+};
+
+export const useSyncStore = createPersistStore(
+  DEFAULT_SYNC_STATE,
   (set, get) => ({
-    formatVersion(version: string) {
-      if (get().versionType === "date") {
-        version = formatVersionDate(version);
-      }
-      return version;
+    cloudSync() {
+      const config = get()[get().provider];
+      return Object.values(config).every((c) => c.toString().length > 0);
     },
 
-    async getLatestVersion(force = false) {
-      const versionType = get().versionType;
-      let version =
-        versionType === "date"
-          ? getClientConfig()?.commitDate
-          : getClientConfig()?.version;
-
-      set(() => ({ version }));
-
-      const shouldCheck = Date.now() - get().lastUpdate > 2 * 60 * ONE_MINUTE;
-      if (!force && !shouldCheck) return;
-
-      set(() => ({
-        lastUpdate: Date.now(),
-      }));
-
-      try {
-        const remoteId = await getVersion(versionType);
-        set(() => ({
-          remoteVersion: remoteId,
-        }));
-        if (window.__TAURI__?.notification && isApp) {
-          // Check if notification permission is granted
-          await window.__TAURI__?.notification
-            .isPermissionGranted()
-            .then((granted) => {
-              if (!granted) {
-                return;
-              } else {
-                // Request permission to show notifications
-                window.__TAURI__?.notification
-                  .requestPermission()
-                  .then((permission) => {
-                    if (permission === "granted") {
-                      if (version === remoteId) {
-                        // Show a notification using Tauri
-                        window.__TAURI__?.notification.sendNotification({
-                          title: "NextChat",
-                          body: `${Locale.Settings.Update.IsLatest}`,
-                          icon: `${ChatGptIcon.src}`,
-                          sound: "Default",
-                        });
-                      } else {
-                        const updateMessage =
-                          Locale.Settings.Update.FoundUpdate(`${remoteId}`);
-                        // Show a notification for the new version using Tauri
-                        window.__TAURI__?.notification.sendNotification({
-                          title: "NextChat",
-                          body: updateMessage,
-                          icon: `${ChatGptIcon.src}`,
-                          sound: "Default",
-                        });
-                      }
-                    }
-                  });
-              }
-            });
-        }
-        console.log("[Got Upstream] ", remoteId);
-      } catch (error) {
-        console.error("[Fetch Upstream Commit Id]", error);
-      }
+    markSyncTime() {
+      set({ lastSyncTime: Date.now(), lastProvider: get().provider });
     },
 
-    async updateUsage(force = false) {
-      // only support openai for now
-      const overOneMinute = Date.now() - get().lastUpdateUsage >= ONE_MINUTE;
-      if (!overOneMinute && !force) return;
+    export() {
+      const state = getLocalAppState();
+      const datePart = isApp
+        ? `${new Date().toLocaleDateString().replace(/\//g, "_")} ${new Date()
+            .toLocaleTimeString()
+            .replace(/:/g, "_")}`
+        : new Date().toLocaleString();
 
-      set(() => ({
-        lastUpdateUsage: Date.now(),
-      }));
+      const fileName = `Backup-${datePart}.json`;
+      downloadAs(JSON.stringify(state), fileName);
+    },
+
+    async import() {
+      const rawContent = await readFromFile();
 
       try {
-        const api = new ClientApi(ModelProvider.GPT);
-        const usage = await api.llm.usage();
-
-        if (usage) {
-          set(() => ({
-            used: usage.used,
-            subscription: usage.total,
-          }));
-        }
+        const remoteState = JSON.parse(rawContent) as AppState;
+        const localState = getLocalAppState();
+        mergeAppState(localState, remoteState);
+        setLocalAppState(localState);
+        location.reload();
       } catch (e) {
-        console.error((e as Error).message);
+        console.error("[Import]", e);
+        showToast(Locale.Settings.Sync.ImportFailed);
       }
+    },
+
+    getClient() {
+      const provider = get().provider;
+      const client = createSyncClient(provider, get());
+      return client;
+    },
+
+    async sync() {
+      const localState = getLocalAppState();
+      const provider = get().provider;
+      const config = get()[provider];
+      const client = this.getClient();
+
+      try {
+        const remoteState = JSON.parse(
+          await client.get(config.username),
+        ) as AppState;
+        mergeAppState(localState, remoteState);
+        setLocalAppState(localState);
+      } catch (e) {
+        console.log("[Sync] failed to get remote state", e);
+      }
+
+      await client.set(config.username, JSON.stringify(localState));
+
+      this.markSyncTime();
+    },
+
+    async check() {
+      const client = this.getClient();
+      return await client.check();
     },
   }),
   {
-    name: StoreKey.Update,
-    version: 1,
+    name: StoreKey.Sync,
+    version: 1.1,
+
+    migrate(persistedState, version) {
+      const newState = persistedState as typeof DEFAULT_SYNC_STATE;
+
+      if (version < 1.1) {
+        newState.upstash.username = STORAGE_KEY;
+      }
+
+      return newState as any;
+    },
   },
 );
