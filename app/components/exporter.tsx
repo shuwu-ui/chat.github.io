298d9abf804e6ff4c456e54e3e29620cf7880a79
shiwu <152254394+shuwu-ui@users.noreply.github.com>
2024-02-22 13:31:24 +0800

    Update exporter.tsx

--- app/components/exporter.tsx
+++ app/components/exporter.tsx
@@ -544,9 +544,9 @@ export function ImagePreviewer(props: {
           </div>
 
           <div>
-            <div className={styles["main-title"]}>NextChat</div>
+            <div className={styles["main-title"]}>Shawn AI</div>
             <div className={styles["sub-title"]}>
-              github.com/Yidadaa/ChatGPT-Next-Web
+              chat-free.icu
             </div>
             <div className={styles["icons"]}>
               <ExportAvatar avatar={config.avatar} />
